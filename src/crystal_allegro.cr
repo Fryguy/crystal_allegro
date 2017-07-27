4d2b259e024b100974136da443c26df3ec5b3e55
Jason Frey <fryguy9@gmail.com>
2017-07-26 23:18:38 -0400

    Add initial methods to bootstrap an application.

--- src/crystal_allegro.cr
+++ src/crystal_allegro.cr
@@ -1,5 +1,21 @@
 require "./crystal_allegro/*"
 
 module CrystalAllegro
-  # TODO Put your code here
+  @@real_main : Proc(Int32, Array(String), Nil)?
+
+  def self.run_main(@@real_main)
+    LibAllegro.run_main(0, nil, ->run_main_callback)
+  end
+
+  def self.run_main_callback(argc : LibC::Int, argv : LibC::Char**) : LibC::Int
+    @@real_main.not_nil!.call(ARGV.size + 1, ARGV.unshift($0))
+    0
+  rescue ex
+    STDERR.puts ex.inspect_with_backtrace
+    1
+  end
+
+  def self.init
+    LibAllegro.install_system(LibAllegro.get_allegro_version, nil)
+  end
 end
