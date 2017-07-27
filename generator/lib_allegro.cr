98c47e1db87a1d024c606f9e06a353122c0532fc
Jason Frey <fryguy9@gmail.com>
2017-07-26 23:17:52 -0400

    Add initial generator

---
+++ generator/lib_allegro.cr
@@ -0,0 +1,13 @@
+@[Include(
+  "allegro5/allegro.h",
+  prefix: %w(al_ AL_ ALLEGRO_ _ALLEGRO)
+)]
+@[Link("allegro")]
+{% if flag?(:darwin) %}
+  @[Link(ldflags: "-L`xcode-select --print-path`/usr/lib")]
+  @[Link(ldflags: "-rpath `xcode-select --print-path`/usr/lib")]
+{% else %}
+  @[Link(ldflags: "`llvm-config-3.6 --ldflags 2>/dev/null || llvm-config-3.5 --ldflags 2>/dev/null || llvm-config --ldflags 2>/dev/null`")]
+{% end %}
+lib LibAllegro
+end
